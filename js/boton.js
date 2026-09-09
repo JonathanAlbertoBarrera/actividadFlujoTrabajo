d49265d5652890a351ee8d9ebe40b1cb67f6ecdd
Daniel-Aguilar-Dev <20233tn088@utez.edu.mx>
2026-09-08 18:17:23 -0600

    [ADD] funciones del boton del caos

---
+++ js/boton.js
@@ -0,0 +1,12 @@
+function colorAleatorio() {
+    const letras = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letras[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function cambiarColor(){
+    colorAleatorio();
+}
